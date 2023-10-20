d83d922706b87f6ee16945b07be768d9b9f1323d
zaidmajid <zaidmajid2345@gmail.com>
2023-10-20 00:46:52 -0700

    zaid fourth PR

---
+++ Python/Hard/validating_postalcode.py
@@ -0,0 +1,5 @@
+## muhammad zaid majid  
+## https://www.hackerrank.com/challenges/validating-postalcode/problem
+
+regex_integer_in_range = r"^[1-9]\d{5}$"
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"
