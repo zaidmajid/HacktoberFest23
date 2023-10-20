6fa4a3f0e059a6742c5ce867fb26dab3187378b1
Syed Abdul Rehman <syed.abdul.rehman.idrees@gmail.com>
2023-10-20 11:17:03 +0500

    Directory structure

---
+++ C++/Easy/hello-world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Solved By: Syed Abdul Rehman
+URL: https://www.hackerrank.com/challenges/cpp-hello-world/problem
+*/
+
+int main() {
+    cout << "Hello, World" << endl;
+    return 0;
+}
