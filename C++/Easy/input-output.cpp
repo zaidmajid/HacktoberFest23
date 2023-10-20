b130c9a656b6491461ab0be43a99869c5a6058e2
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2023-10-20 11:41:22 +0500

    Hackerrank 2

---
+++ C++/Easy/input-output.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+Author: Wali Ahmad
+URL: https://www.hackerrank.com/challenges/cpp-input-and-output/
+*/
+
+int main()
+{
+    int x, y, z, s;
+    cin >> x >> y >> z;
+    s = x + y + z;
+    cout << s;
+    return 0;
+}
